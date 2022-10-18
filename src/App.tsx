68c121f207b61b9a813c351ee6af79e400a79eac
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-10-18 09:53:58 -0300

    feat: add squirtle image
    Replace React logo with an image of squirtle.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <main className="App-header">
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <img
+            alt="Squirtle happily walking with arms and legs wide open"
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/7.svg"
+          />
+          <br />
+          Welcome!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </main>
     </div>
   );
 }
