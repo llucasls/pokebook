3d0e9ea4f8ceb9d5834fc3490297882e513ce397
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-10-20 15:04:51 -0300

    feat: create function to convert stat name

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -5,3 +5,16 @@ type UseState<T> = [T, Dispatch<SetStateAction<T>>];
 export function useState(initialState: any) {
   return reactUseState(initialState) as UseState<typeof initialState>;
 }
+
+const stats: { [key: string]: string } = {
+  hp: 'HP',
+  attack: 'Attack',
+  defense: 'Defense',
+  'special-attack': 'Sp. Atk',
+  'special-defense': 'Sp. Def',
+  speed: 'Speed',
+};
+
+export function getStat(name: string) {
+  return stats[name];
+}
