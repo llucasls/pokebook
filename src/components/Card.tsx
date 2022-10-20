c6ce7084a41eda681dd7f157cd2a8da408d5d5a2
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-10-20 17:58:32 -0300

    feat: create renderPokemonStats function

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,6 +6,10 @@ const renderPokemonTypes = (types: PokemonType[]) =>
   types.map(({ type }, index) =>
     <span key={ index }>{ type.name }</span>);
 
+const renderPokemonStats = (stats: PokemonStat[]) =>
+  stats.map(({ stat, base_stat }, index) =>
+    <div key={ index }><span>{getStat(stat.name)}: {base_stat}</span><br /></div>);
+
 const Card = ({ id }: { id: number }) => {
   const [pokemon, setPokemon] = useState({});
   const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
@@ -19,9 +23,7 @@ const Card = ({ id }: { id: number }) => {
       <img src={ pokemon.sprites.front_default } alt={ `${pokemon.name} facing the player` } />
       <div>{ pokemon.name }</div>
       <div>{ renderPokemonTypes(pokemon.types) }</div>
-      <div>
-        { pokemon.stats.map(({ base_stat, stat }: PokemonStat) => <><span>{getStat(stat.name)}: {base_stat}</span><br /></>) }
-      </div>
+      <div>{ renderPokemonStats(pokemon.stats) }</div>
     </div>
   );
   return <div>{ null }</div>;
