0bc9e8ae1d7dfc50a9c94fbe320284b1f5eab3e4
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-10-20 15:07:02 -0300

    feat: add more information on each pokemon

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,7 @@
-import { useState } from '../helpers';
+import { useState, getStat } from '../helpers';
+import { PokemonType, PokemonStat } from '../types/pokemon';
 import './Card.css';
+
 
 const Card = ({ id }: { id: number }) => {
   const [pokemon, setPokemon] = useState({});
@@ -8,7 +10,22 @@ const Card = ({ id }: { id: number }) => {
     .then((binData) => binData.json())
     .then(setPokemon);
 
-  if (pokemon.name) return <div className="card">{ pokemon.name }</div>;
+  if (pokemon.name) return (
+    <div className="card">
+      <div>{ pokemon.id }</div>
+      <img src={ pokemon.sprites.front_default } alt={ `${pokemon.name} facing the player` } />
+      <div>{ pokemon.name }</div>
+      <div>
+        {
+          pokemon.types
+            .map(({ type }: PokemonType) => <span>{ type.name }</span>)
+        }
+      </div>
+      <div>
+        { pokemon.stats.map(({ base_stat, stat }: PokemonStat) => <><span>{getStat(stat.name)}: {base_stat}</span><br /></>) }
+      </div>
+    </div>
+  );
   return <div>{ null }</div>;
 };
 
