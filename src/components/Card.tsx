dab9bb8c389ddcffec3dbdbbe5abb1351e338e00
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-10-20 17:57:54 -0300

    feat: create renderPokemonTypes function

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,6 +2,9 @@ import { useState, getStat } from '../helpers';
 import { PokemonType, PokemonStat } from '../types/pokemon';
 import './Card.css';
 
+const renderPokemonTypes = (types: PokemonType[]) =>
+  types.map(({ type }, index) =>
+    <span key={ index }>{ type.name }</span>);
 
 const Card = ({ id }: { id: number }) => {
   const [pokemon, setPokemon] = useState({});
@@ -15,12 +18,7 @@ const Card = ({ id }: { id: number }) => {
       <div>{ pokemon.id }</div>
       <img src={ pokemon.sprites.front_default } alt={ `${pokemon.name} facing the player` } />
       <div>{ pokemon.name }</div>
-      <div>
-        {
-          pokemon.types
-            .map(({ type }: PokemonType) => <span>{ type.name }</span>)
-        }
-      </div>
+      <div>{ renderPokemonTypes(pokemon.types) }</div>
       <div>
         { pokemon.stats.map(({ base_stat, stat }: PokemonStat) => <><span>{getStat(stat.name)}: {base_stat}</span><br /></>) }
       </div>
