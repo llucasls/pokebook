2557fa14910319cbe701ac6f99820e3faa870cf0
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-10-21 04:17:53 -0300

    refactor: replace render functions with components

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,13 +2,20 @@ import { useState, getStat } from '../helpers';
 import { PokemonType, PokemonStat } from '../types/pokemon';
 import './PokemonCard.css';
 
-const renderPokemonTypes = (types: PokemonType[]) =>
-  types.map(({ type }) =>
-    <span key={ type.name }>{ type.name }</span>);
+const getTypeName = ({ type }: PokemonType) =>
+  <span>{ type.name }</span>;
 
-const renderPokemonStats = (stats: PokemonStat[]) =>
-  stats.map(({ stat, base_stat }) =>
-    <div key={ stat.name }><span>{getStat(stat.name)}: {base_stat}</span><br /></div>);
+const getStatAttributes = ({ stat, base_stat }: PokemonStat) =>
+  <div key={ stat.name }>
+    <span>{ getStat(stat.name) }: { base_stat }</span>
+    <br />
+  </div>;
+
+const PokemonTypes = ({ children }: { children: PokemonType[] }) =>
+  <div>{ children.map(getTypeName) }</div>;
+
+const PokemonStats = ({ children }: { children: PokemonStat[] }) =>
+  <div>{ children.map(getStatAttributes) }</div>;
 
 function Card({ id }: { id: number }) {
   const [pokemon, setPokemon] = useState({});
@@ -22,8 +29,8 @@ function Card({ id }: { id: number }) {
       <div>{ pokemon.id }</div>
       <img src={ pokemon.sprites.front_default } alt={ `${pokemon.name} facing the player` } />
       <div>{ pokemon.name }</div>
-      <div>{ renderPokemonTypes(pokemon.types) }</div>
-      <div>{ renderPokemonStats(pokemon.stats) }</div>
+      <PokemonTypes>{ pokemon.types }</PokemonTypes>
+      <PokemonStats>{ pokemon.stats }</PokemonStats>
     </div>
   );
   return <div>{ null }</div>;
