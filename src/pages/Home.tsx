fab5fe227de7465ba2a4dc772fbef7f1e6b87a44
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-10-21 19:24:15 -0300

    feat: create Home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom';
+import '../App.css';
+
+function Home() {
+  return (
+    <div className="App">
+      <main className="App-header">
+        <p>
+          <Link to="/pokemonlist">
+            <img
+                alt="Squirtle happily walking with arms and legs wide open"
+                src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/7.svg"
+            />
+          </Link>
+          <br />
+          Welcome!
+        </p>
+      </main>
+    </div>
+  );
+}
+
+export default Home;
