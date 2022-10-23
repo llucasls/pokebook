7b7edeaff140d7fabfc00b8d2fa4d06439a3a818
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-10-22 22:28:00 -0300

    feat: add NavBar to pokemon list page

--- src/pages/PokemonList.tsx
+++ src/pages/PokemonList.tsx
@@ -1,9 +1,11 @@
 import '../App.css';
 import PokemonCard from '../components/PokemonCard';
+import NavBar from '../components/NavBar';
 
 function PokemonList() {
   return (
     <div className="App">
+      <NavBar />
       <main className="App-header">
         { Array(12).fill(0).map((_, index) => (<PokemonCard id={index + 1} key={index + 1} />)) }
       </main>
