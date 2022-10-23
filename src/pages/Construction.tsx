2c5aeb87e2fb1aa16d7d194a37ef354ea4a1e52a
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-10-22 23:47:57 -0300

    feat: create Construction component

---
+++ src/pages/Construction.tsx
@@ -0,0 +1,19 @@
+import NavBar from '../components/NavBar';
+
+function Construction() {
+  return (
+    <div className="App">
+      <NavBar />
+      <main className="App-header">
+        <img
+          width="35%"
+          src="gurdurr.jpg"
+          alt="Gurdurr holding a steel beam above his head"
+        />
+        <h3>Page currently under construction</h3>
+      </main>
+    </div>
+  )
+}
+
+export default Construction;
