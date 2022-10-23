15915fef090d71335314ec21286fbffae5a73990
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-10-22 22:26:47 -0300

    feat: replace string labels with buttons

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,14 @@
+import NavButton from './NavButton';
+
 function NavBar() {
   return (
     <nav>
-      Home
-      Pokemon List
-      Details
-      Routes
-      Moves
-      Evolutions
+      <NavButton label="Home" path="/" />
+      <NavButton label="Pokemon List" path="/pokemonlist" />
+      <NavButton label="Details" path="/" />
+      <NavButton label="Routes" path="/" />
+      <NavButton label="Moves" path="/" />
+      <NavButton label="Evolutions" path="/" />
     </nav>
   );
 }
