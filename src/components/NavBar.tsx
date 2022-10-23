6bbeab351693c5eb406c89056c2eba92a7dc74de
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-10-22 22:04:05 -0300

    feat: create NavBar component

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,14 @@
+function NavBar() {
+  return (
+    <nav>
+      Home
+      Pokemon List
+      Details
+      Routes
+      Moves
+      Evolutions
+    </nav>
+  );
+}
+
+export default NavBar;
