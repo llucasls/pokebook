491cb7f746489fb0d1cf71d5c6cf61e78a61da3d
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-10-24 20:05:42 -0300

    feat: redirect when pokemon not found

--- src/pages/Pokemon.tsx
+++ src/pages/Pokemon.tsx
@@ -1,14 +1,16 @@
 import axios from 'axios';
 import { useState } from '../helpers';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 
 function Pokemon() {
   const { id } = useParams();
+  const navigate = useNavigate();
   const [pokemon, setPokemon] = useState({});
   const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
   axios(url)
     .then((response) => response.data)
-    .then(setPokemon);
+    .then(setPokemon)
+    .catch(() => navigate("/404"));
 
   if (pokemon.name) return <span>name: { pokemon.name }</span>;
   return <span />;
