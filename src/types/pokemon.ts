fc06107ee452bd8217409a8ce41f9d272ff122d6
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-10-26 00:49:51 -0300

    feat: add type definitions for pokemon

--- src/types/pokemon.ts
+++ src/types/pokemon.ts
@@ -1,3 +1,129 @@
+export interface PokemonAbility {
+  ability: {
+    name: string;
+    url: string;
+  }
+  is_hidden: boolean;
+  slot: number;
+}
+
+export type PokemonBaseExperience = number;
+
+export interface PokemonForm {
+  name: string;
+  url: string;
+}
+
+export interface PokemonGameIndex {
+  game_index: number;
+  version: {
+    name: string;
+    url: string;
+  }
+}
+
+export type PokemonHeight = number;
+
+export type PokemonHeldItem = [];
+
+export type PokemonId = number;
+
+export type PokemonIsDefault = boolean;
+
+export type PokemonLocationAreaEncounters = string;
+
+interface VersionGroup {
+  level_learned_at: number;
+  move_learn_method: {
+    name: string;
+    url: string;
+  }
+  version_group: {
+    name: string;
+    url: string;
+  }
+}
+
+export interface PokemonMove {
+  move: {
+    name: string;
+    url: string;
+  }
+  version_group_details: VersionGroup[];
+}
+
+export type PokemonName = string;
+
+export type PokemonOrder = number;
+
+export type PokemonPastTypes = [];
+
+export interface PokemonSpecies {
+  name: string;
+  url: string;
+}
+
+export interface PokemonSprites {
+  back_default: string;
+  back_female: string | null;
+  back_shiny: string;
+  back_shiny_female: string | null;
+  front_default: string;
+  front_female: string | null;
+  front_shiny: string;
+  front_shiny_female: string | null;
+  other: {
+    dream_world: {
+      front_default: string;
+      front_female: string | null;
+    }
+    home: {
+      front_default: string;
+      front_female: string | null;
+      front_shiny: string;
+      front_shiny_female: string | null;
+    }
+    "official-artwork": {
+      front_default: string;
+    }
+  }
+  versions: {
+    "generation-i": {
+      "red-blue": object;
+      yellow: object;
+    }
+    "generation-ii": {
+      crystal: object;
+      gold: object;
+      silver: object;
+    }
+    "generation-iii": {
+      emerald: object;
+      "firered-leafgreen": object;
+      "ruby-sapphire": object;
+    }
+    "generation-iv": {
+      "diamond-pearl": object;
+      "heartgold-soulsilver": object;
+      platinum: object;
+    }
+    "generation-v": {
+      "black-white": object;
+    }
+    "generation-vi": {
+      "omegaruby-alphasapphire": object;
+      "x-y": object;
+    }
+    "generation-vii": {
+      icons: object;
+      "ultra-sun-ultra-moon": object;
+    }
+    "generation-viii": {
+      icons: object;
+    }
+  }
+}
+
 export interface PokemonStat {
   base_stat: number;
   effort: number;
@@ -14,3 +140,5 @@ export interface PokemonType {
     url: string;
   }
 }
+
+export type PokemonWeight = number;
