e9579bc264b8b3ac9229f51aabc8f5ca9b09ea91
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-11-10 16:02:02 -0300

    feat: create component PokemonDetailsCard

---
+++ src/components/PokemonDetailsCard.tsx
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import { useState } from '../helpers';
+import './PokemonCard.css';
+import PokemonTypes from './PokemonTypes';
+import PokemonStats from './PokemonStats';
+
+function PokemonDetailsCard({ id }: { id: number }) {
+  const [pokemon, setPokemon] = useState({});
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+  axios(url)
+    .then((response) => response.data)
+    .then(setPokemon);
+
+  if (pokemon.name) return (
+    <div className="card">
+      <div>{ pokemon.id }</div>
+      <img src={ pokemon.sprites.front_default } alt={ `${pokemon.name} facing the player` } />
+      <div>{ pokemon.name }</div>
+      <PokemonTypes types={ pokemon.types } />
+      <PokemonStats stats={ pokemon.stats } />
+    </div>
+  );
+  return <div>{ null }</div>;
+};
+
+export default PokemonDetailsCard;
