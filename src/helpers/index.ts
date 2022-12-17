c65addc53e1dbca9e0014b9ef22b796512591ca7
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-12-17 19:11:35 -0300

    feat: export type for use in components

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,6 +1,6 @@
 import { useState as reactUseState, Dispatch, SetStateAction } from 'react';
 
-type UseStateHook<T> = [T, Dispatch<SetStateAction<T>>];
+export type UseStateHook<T> = [T, Dispatch<SetStateAction<T>>];
 
 export function useState(initialState: any) {
   return reactUseState(initialState) as UseStateHook<typeof initialState>;
