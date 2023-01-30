795ecca4460beab3536f9e5f7de52d03757dbcd8
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-01-30 20:42:52 -0300

    feat: change navigation bar style

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,14 +1,17 @@
 import NavButton from '../NavButton';
+import "./style.css";
 
 function NavBar() {
   return (
     <nav>
-      <NavButton label="Home" path="/" />
-      <NavButton label="Pokemon List" path="/pokemonlist" />
-      <NavButton label="Details" path="/details" />
-      <NavButton label="Routes" path="/routes" />
-      <NavButton label="Moves" path="/moves" />
-      <NavButton label="Evolutions" path="/evolutions" />
+      <div>
+        <NavButton label="Home" path="/" />
+        <NavButton label="Pokemon List" path="/pokemonlist" />
+        <NavButton label="Details" path="/details" />
+        <NavButton label="Routes" path="/routes" />
+        <NavButton label="Moves" path="/moves" />
+        <NavButton label="Evolutions" path="/evolutions" />
+      </div>
     </nav>
   );
 }
